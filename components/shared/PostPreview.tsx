f37ef594d036d392c386d785544850f005e1839f
Oleksandr Boiko <iccc97@gmail.com>
2024-12-02 21:18:19 +0200

    add Suspense

--- components/shared/PostPreview.tsx
+++ components/shared/PostPreview.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export function PostPreview() {
   const searchParams = useSearchParams()
@@ -12,19 +13,21 @@ export function PostPreview() {
   const candidateLevel = searchParams.get('candidateLevel')
 
   return (
-    <pre className='rounded-md bg-foreground text-background p-4'>
-      <code>
-        Name: {name}
-        <br />
-        Email: {email}
-        <br />
-        Assignment Description: {assignmentDescription}
-        <br />
-        GitHub Repo URL: {githubRepoUrl}
-        <br />
-        Candidate Level: {candidateLevel}
-      </code>
-    </pre>
+    <Suspense fallback={<div>Loading...</div>}>
+      <pre className='rounded-md bg-foreground text-background p-4'>
+        <code>
+          Name: {name}
+          <br />
+          Email: {email}
+          <br />
+          Assignment Description: {assignmentDescription}
+          <br />
+          GitHub Repo URL: {githubRepoUrl}
+          <br />
+          Candidate Level: {candidateLevel}
+        </code>
+      </pre>
+    </Suspense>
   )
 }
 
